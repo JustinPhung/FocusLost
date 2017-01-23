2d0b99b6a673b3d550c3934c215290125045615a
jp <justinbao@hotmail.de>
2017-01-23 11:16:59 +0100

    w

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+  amountChangeEvent: number = 0;
+
+  title = 'Dynamic Schema Focus Lost';
+
+  data: any = {};
+
+  schema: any = {
+    "$schema": "http://json-schema.org/draft-04/schema#",
+    "type": "object",
+    "properties": {
+      "subspecies": {
+        "type": "string",
+        "title": "Subspecies"
+      }
+    }
+  }
+
+  onChange(event: any) : void {
+
+    this.amountChangeEvent += 1;
+
+    this.data = event;
+    let clone = JSON.parse(JSON.stringify(this.schema));
+
+    clone.properties.subspecies.title = this.amountChangeEvent + " Change Events";
+
+    this.schema = clone;
+    this.data = event;
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {JsonSchemaFormModule} from "angular2-json-schema-form";
 
 @NgModule({
   declarations: [
@@ -12,7 +13,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    JsonSchemaFormModule.forRoot()
+
   ],
   providers: [],
   bootstrap: [AppComponent]
